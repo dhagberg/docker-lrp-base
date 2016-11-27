e8d6a8f47fc61a8a55b77832fd1f8625b623dab7
D. J. Hagberg <dhagberg@millibits.com>
2016-11-27 02:58:18 +0000

    fix BUILDTS, missing openssl, which

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -xe
 # Get latest centos
 docker pull centos:centos6
+# Update timestamp
+buildts=$(TZ=UTC date "+%Y-%m-%dT%H:%M:%SZ")
+sed -e "s/@BUILDTS@/${buildts}/g" Dockerfile.in > Dockerfile
 # Run build
 docker build -t dhagberg/co811-base .
+/bin/rm -f Dockerfile
