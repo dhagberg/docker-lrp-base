089ecd2bc3598be163cdecd7feee85bfeee54ebc
D. J. Hagberg <dhagberg@millibits.com>
2016-11-27 01:32:58 +0000

    no need to run sudo with docker group

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 # Get latest centos
-sudo docker pull centos:centos6
+docker pull centos:centos6
 # Run build
-sudo docker build -t dhagberg/co811-base .
+docker build -t dhagberg/co811-base .
