25705ed777654db3b770e6f1a1d174b386608aac
D. J. Hagberg <dhagberg@millibits.com>
2020-08-29 23:21:32 +0000

    Add support for tagging/pushing to both docker hub and local registry

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ sed -e "s/@BUILDTS@/${buildts}/g" Dockerfile.in > Dockerfile
 # Run build
 docker build -t dhagberg/co811-base .
 /bin/rm -f Dockerfile
+# Tag and push if given
+if [ "$1" = "push" ]; then
+    docker push dhagberg/co811-base
+    docker tag  dhagberg/co811-base svn.co811.org:5000/co811-base
+    docker push svn.co811.org:5000/co811-base
+fi
