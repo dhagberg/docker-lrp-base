5528503b35fa49b830bca5c56daeb420cb07ed5b
D. J. Hagberg <dhagberg@millibits.com>
2024-09-06 10:12:33 -0600

    Switch to Unbuntu 24.04 base and commands (#1)
    * Switch to Unbuntu 24.04 base and commands
    
    * Use apt-get in lieu of plain (unable) apt cli
    
    * Add Zulu repo apt sources/gpg key bits for lrp-java and friends.
    
    * Add -u- delimiter to be able to publish these images alongside legacy centos

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 buildts=$(TZ=UTC date "+%Y-%m-%dT%H:%M:%SZ")
 sed -e "s/@BUILDTS@/${buildts}/g" Dockerfile.in > Dockerfile
 
-PLAIN_NAME=lrp-base
+PLAIN_NAME=lrp-u-base
 HUB_NAME=dhagberg/$PLAIN_NAME
 CI_NAME=cga-ci:5000/$PLAIN_NAME
 
@@ -13,8 +13,10 @@ docker build --pull -t $HUB_NAME .
 /bin/rm -f Dockerfile
 
 # Tag and push if given
-if [ "$1" = "push" ]; then
-    docker push $HUB_NAME
-    docker tag  $HUB_NAME $CI_NAME
-    docker push $CI_NAME
+if [ $# -ge 1 ]; then
+    if [ "$1" = "push" ]; then
+        docker push $HUB_NAME
+        docker tag  $HUB_NAME $CI_NAME
+        docker push $CI_NAME
+    fi
 fi
